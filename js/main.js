577c02b76ea8d1de55d4561b02c3230f2147f095
CarloKottier <carlokottier@gmail.com>
2020-02-21 15:58:28 +0100

    Added Logo in body

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 //Navbar 
 
 //Logo
-var img = document.createElement('img');
-img.src = "../Images/logo.png";
+function show_image(src, width, height, alt) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
 
-//simg.setAttribute('src', '../Images/logo.png')
-//img.appendChild(img);s
-document.appendChild(img);
+    // This next line will just add it to the <body> tag
+    document.body.appendChild(img);
+}
+
+var img = class {
+    constructor(src) {
+        this.src = src;
+        this.show = show_image(src, 200, 100, "Random alt");
+    }
+};
+
+logo1 = new img('./Images/logo.png');
 
 
 
