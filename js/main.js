5beb342abc70c2059859fe851fe28d3aa61cc365
Bavlly Fanous <bavlly@backbase.com>
2020-02-25 20:42:51 +0100

    refactor code

--- js/main.js
+++ js/main.js
@@ -1,26 +1,4 @@
-//Navbar 
 
-
-//Logo
-function show_image(src, width, height, alt) {
-    var img = document.createElement("img");
-    img.src = src;
-    img.width = width;
-    img.height = height;
-    img.alt = alt;
-
-    // This next line will just add it to the <body> tag
-    document.body.appendChild(img);
-}
-
-var img = class {
-    constructor(src) {
-        this.src = src;
-        this.show = show_image(src, 200, 100, "Random alt");
-    }
-};
-
-logo1 = new img('./Images/logo.png');
 
 
 //content
@@ -80,14 +58,4 @@ function TableSupport(text)
  return td;
 }
 document.body.appendChild(table);
-
-
-
-
-//footer
-var footer = document.createElement('footer');
-var footerText = document.createTextNode('\251 Utrecht University');
-
-footer.appendChild(footerText);
-document.body.appendChild(footer);
 
