6d3d8777ce2bf81beecc5eb70aa1bb2b13c2c4f4
melvin <Melvin.Leijdsman@gmail.com>
2020-02-25 21:19:09 +0100

    table improve

--- js/main.js
+++ js/main.js
@@ -70,51 +70,51 @@ let table = create('table');
 let thead = create('thead');
 let tbody = create('tbody');
 
-function addTR (...tds) {
+function addTR (twhat,tag,tds) {
+  
+  let tr = create("tr");  
 
-  let tr = create('tr');   
   for (let text of tds) {
-    let td = create("td");
+    let td = create(tag);
     appendText(td,text);
     append(tr,td);
   }
   
-  append(tbody,tr);
+  append(twhat,tr);
 }
 
+function addMultiple(twhat,tag, tds){
+   for (let ts of tds)
+    addTR(twhat,tag,ts);
+}
 
-let trhead = create('tr');
+addTR(thead,"th",["Week","Date","Topics","Materials","Deadlines"])
+addMultiple(tbody,"td",[
+  ["46", "12/11", "Introduction","",""],
+  [  "", "14/11", "Haskell refresh","",""],
+  ["47", "19/11", "Threads","",""],
+  [  "", "21/11", "Mvars", "Chapter 7(plus parts of 8 & 9",""],
+  ["48", "26/11", "Software Transactional Memory(1)", "Chapter 10",""],
+  [  "", "28/11", "Software Transactional Memory(2)", "Chapter 10",""],
+  [  "", "30/11", "", "", "Assingment 1"],
+  ["49",  "3/12", "Netchange", "Netchange Algorithm, Chapter 12",""],
+  [  "",  "5/12", "Parallelism from Concurrency", "Chapter 13",""],
+  ["50", "10/12", "Work & Span","",""],
+  [  "", "12/12", "No lecture","",""],
+  ["51", "17/12", "Revision","",""],
+  [  "", "19/12", "Mid-session exam","",""],
+  [ "1",   "4/1", "", "", "Assingment 2"],
+  [ "2",   "7/1", "Data Parrallelism(1)", "Part 1 Introduction, chapter 5 & 6",""],
+  [  "",  "9/11", "Data Parallelism(2)","",""],
+  [ "3", "14/11", "Data Parellelism(3)","",""],
+  [  "",  "16/1", "Scan","",""],
+  [ "4",  "21/1", "GPCPU","",""],
+  [  "",  "23/1", "Conclusion","",""],
+  [ "5",  "30/1", "Final exam","",""],
+  [  "",   "1/2", "No lecture", "", "Assingment 3"]
+]);
 
-for (let text of ["Week","Date","Topics","Materials","Deadlines"]){
-    let th = create('th');
-    appendText(th,text);
-    append(trhead,th);
-} 
-append(thead,trhead);
 append(table,thead);
-
-addTR("46","12/11","Introduction","","");
-addTR("","14/11","Haskell refresh","","");
-addTR("47","19/11","Threads","","");
-addTR("","21/11","Mvars","Chapter 7(plus parts of 8 & 9","");
-addTR("48","26/11","Software Transactional Memory(1)","Chapter 10","");
-addTR("","28/11","Software Transactional Memory(2)","Chapter 10","");
-addTR("","30/11","","","Assingment 1");
-addTR("49","3/12","Netchange","Netchange Algorithm, Chapter 12","");
-addTR("","5/12","Parallelism from Concurrency","Chapter 13","");
-addTR("50","10/12","Work & Span","","");
-addTR("","12/12","No lecture","","");
-addTR("51","17/12","Revision","","");
-addTR("","19/12","Mid-session exam","","");
-addTR("1","4/1","","","Assingment 2");
-addTR("2","7/1","Data Parrallelism(1)","Part 1 Introduction, chapter 5 & 6","");
-addTR("","9/11","Data Parallelism(2)","","");
-addTR("3","14/11","Data Parellelism(3)","","");
-addTR("","16/1","Scan","","");
-addTR("4","21/1","GPCPU","","");
-addTR("","23/1","Conclusion","","");
-addTR("5","30/1","Final exam","","");
-addTR("","1/2","No lecture","","Assingment 3");
 append(table,tbody)
 append(body,table);
 
