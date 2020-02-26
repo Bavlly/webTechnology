2900d224b8512e886ee048594ba53c84c4ffd9db
Bavlly Fanous <bavlly@backbase.com>
2020-02-26 15:59:23 +0100

    Refactor

--- resources/js/header.js
+++ resources/js/header.js
@@ -1,29 +1,4 @@
-// //Navbar
-//
-//
-// //Logo
-// function show_image(src, width, height, alt) {
-//     var img = document.createElement("img");
-//     img.src = src;
-//     img.width = width;
-//     img.height = height;
-//     img.alt = alt;
-//
-//     // This next line will just add it to the <body> tag
-//     document.body.appendChild(img);
-// }
-//
-// var img = class {
-//     constructor(src) {
-//         this.src = src;
-//         this.show = show_image(src, 200, 100, "Random alt");
-//     }
-// };
-//
-// logo1 = new img('./images/logo.png');
-
-
-//logo 1
+//header
 function show_img(src, width, height, alt){
     var img = document.createElement('img');
     img.src = src;
