2120c41d050a9f2b4e110fd3fbf210da22b27ee0
Bavlly Fanous <bavlly@backbase.com>
2020-02-28 13:27:04 +0100

    Refactor code + added some items to table

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,17 +9,19 @@ let h2 = create('h1');
 appendText(h2,"Find here all the information about the course");
 append(body, h2);
 
+// Create a <p> element
+var x = document.createElement("P");
+// Create a text node
+var t = document.createTextNode("This is a paragraph.");
 
-var x = document.createElement("P");                        // Create a <p> element
-var t = document.createTextNode("This is a paragraph.");    // Create a text node
-
-x.appendChild(t); 
-body.appendChild(x);                                          // Append the text to <p>
+x.appendChild(t);
+// Append the text to <p>
+body.appendChild(x);
 
 
 //class Courses
 class Courses{
-  constructor(title, code, period, ects, nivo) {
+  constructor(title, code, period, ects, nivo, teacher) {
     this.title = title;
     this.code = code;
     this.period = period;
@@ -62,6 +64,27 @@ class Schedule{
 
 // class course werkt
 var subject1 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject2 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject3 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject4 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject5 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject6 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject7 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject8 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject9 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject10 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject11 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject12 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject13 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject14 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject15 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject16 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject17 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject18 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject19 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+var subject20 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+
+
 var information = create('p');
 var subject = createText(subject1.period);
 
@@ -74,14 +97,6 @@ var teacher = createText(teacherassistent.grade);
 
 append(info, teacher);
 append(body, info);
-
-// concurrency1 = new Courses("Concurrency", "INFOB3CC", "Trevor", "2", "C", "7.5", "3");
-// var information = create('p');
-// var subject = createText(concurrency1.teacher);
-//
-// append(information, subject);
-// append(body, information);
-
 
 //creating table
 let table = create('table');
@@ -106,30 +121,12 @@ function addMultiple(twhat,tag, tds){
     addTR(twhat,tag,ts);
 }
 
-addTR(thead,"th",["Week","Date","Topics","Materials","Deadlines"])
+addTR(thead,"th",["Period","Timeslot","Code","Nivo","ects", "vak"]);
 addMultiple(tbody,"td",[
-  ["46", "12/11", "Introduction","",""],
-  [  "", "14/11", "Haskell refresh","",""],
-  ["47", "19/11", "Threads","",""],
-  [  "", "21/11", "Mvars", "Chapter 7(plus parts of 8 & 9",""],
-  ["48", "26/11", "Software Transactional Memory(1)", "Chapter 10",""],
-  [  "", "28/11", "Software Transactional Memory(2)", "Chapter 10",""],
-  [  "", "30/11", "", "", "Assingment 1"],
-  ["49",  "3/12", "Netchange", "Netchange Algorithm, Chapter 12",""],
-  [  "",  "5/12", "Parallelism from Concurrency", "Chapter 13",""],
-  ["50", "10/12", "Work & Span","",""],
-  [  "", "12/12", "No lecture","",""],
-  ["51", "17/12", "Revision","",""],
-  [  "", "19/12", "Mid-session exam","",""],
-  [ "1",   "4/1", "", "", "Assingment 2"],
-  [ "2",   "7/1", "Data Parrallelism(1)", "Part 1 Introduction, chapter 5 & 6",""],
-  [  "",  "9/11", "Data Parallelism(2)","",""],
-  [ "3", "14/11", "Data Parellelism(3)","",""],
-  [  "",  "16/1", "Scan","",""],
-  [ "4",  "21/1", "GPCPU","",""],
-  [  "",  "23/1", "Conclusion","",""],
-  [ "5",  "30/1", "Final exam","",""],
-  [  "",   "1/2", "No lecture", "", "Assingment 3"]
+    [subject1.period, subject1.period, subject1.code, subject1.nivo, subject1.ects, subject1.title],
+    [subject1.period, subject1.period, subject1.code, subject1.nivo, subject1.ects, subject1.title],
+    [subject1.period, subject1.period, subject1.code, subject1.nivo, subject1.ects, subject1.title],
+
 ]);
 
 append(table,thead);
