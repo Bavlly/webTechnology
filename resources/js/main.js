8eeae609c96348c0addaf028d28efcb13bb72d7c
MelvinLeijdsman <melvin.leijdsman@gmail.com>
2020-02-28 14:49:31 +0100

    objects

--- resources/js/main.js
+++ resources/js/main.js
@@ -45,9 +45,9 @@ class Teacher{
 }
 // inheritance from class Teacher
 class TA extends Teacher{
-  constructor(subject, grade, year) {
-    super();
-    this.subject = subject;
+  constructor(first, last, age, gender, interest, subject, grade, year) {
+    super(first,last,age,gender,interest,subject);
+    
     this.grade = grade;
     this.year = year;
   }
@@ -63,26 +63,37 @@ class Schedule{
 }
 
 // class course werkt
-var subject1 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject2 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject3 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject4 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject5 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject6 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject7 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject8 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject9 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject10 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject11 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject12 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject13 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject14 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject15 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject16 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject17 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject18 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject19 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
-var subject20 = new Courses("Concurrency", "INFOB3CC", "3", "7.5", "3");
+
+var teacher1 = new Teacher("Remco","Veltkamp","50","male","Taking long walks on the beach","Gameprogrammeren");
+var teacher2 = new Teacher("Ad","Feelders","40","male","Mining away","Datamining");
+var teacher3 = new Teacher("Trevor","Mcdonell","28","male","Spending time with any dog, he really loves dogs","Concurency");
+var teacher4 = new Teacher("Silja","Renooij","35","female","Making robots","Gameprogrammeren");
+var teacher5 = new Teacher("Hans","Philipi","50","male","Talking","Databases");
+var teacher6 = new Teacher("Erik jan","van Leeuwen","30","male","Dynamic programming","Algoritmiek");
+var teacher7 = new Teacher("Gerard","Tel","60","male","Count sort","Datastructuren");
+var teacher8 = new Teacher("Jacco","Bikker","40","male","Building raytracers","Graphics");
+
+var ta1 = new TA("Tim","Schagen","20","male","Gamen","Gameprogrammeren","9","3");
+var ta2 = new TA("Steven","Langerwerf","25","male","Minen","Datamining","10","m");
+var ta3 = new TA("Hugo","Peters","23","male","threading","Concurrency","9","m");
+var ta4 = new TA("Remi","Wieten","20","male","AI","Kunstmatige intelligentie","9","2");
+var ta5 = new TA("Theo","Harkes","21","male","Storing information","Databases","8","2");
+var ta6 = new TA("Ivo Gabe","de Wolff","22","male","Divide and conquer","Algoritmiek","9","m");
+var ta7 = new TA("Felix","Ant","21","male","Counting with gerard","Datastructuren","9","3");
+var ta8 = new TA("Mark","Dekker","21","male","Pixels","Graphics","9","3");
+
+var subject1 = new Courses("Gameprogrammeren", "INFOB1GP", "1", "7.5", "1",[Teacher1,ta1]);
+var subject2 = new Courses("Datamining", "INFOMDM", "1", "7.5", "m",[Teacher2,ta2]);
+var subject3 = new Courses("Concurrency", "INFOB3CC", "2", "7.5", "3",[Teacher3,ta3]);
+var subject4 = new Courses("Kunstmatige intelligentie", "INFOB2KI", "2", "7.5", "2",[Teacher4,ta4]);
+var subject5 = new Courses("Databases", "INFODB", "3", "7.5", "1",[Teacher5,ta5]);
+var subject6 = new Courses("Algoritmiek", "INFOAL", "3", "7.5", "3",[Teacher6,ta6]);
+var subject7 = new Courses("Datastructuren", "INFODS", "4", "7.5", "2",[Teacher7,ta7]);
+var subject8 = new Courses("Graphics", "INFOGR", "4", "7.5", "2",[Teacher8,ta8]);
+
+
+
+
 
 
 var information = create('p');
