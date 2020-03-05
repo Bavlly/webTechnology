f0c78cd2bff5197f2921c9674f8ece2eb1db50f9
melvin <Melvin.Leijdsman@gmail.com>
2020-03-05 20:25:14 +0100

    tooltip

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,6 +5,7 @@ const append = (a,b) => a.appendChild(b);
 const appendText = (a,str) => a.appendChild(createText(str));
 const body = document.body;
 
+
 let h1 = create('h1');
 appendText(h1,"Find here all the information about the course");
 append(body, h1);
@@ -21,22 +22,23 @@ body.appendChild(x);
 
 //class Courses
 class Courses{
-  constructor(title, code, period, ects, nivo, teacher) {
+  constructor(title, code, period,timeslot, ects, nivo, teacher) {
     this.title = title;
     this.code = code;
     this.period = period;
+    this.timeslot = timeslot;
     this.ects = ects;
     this.nivo = nivo;
+    this.teacher = teacher;
   }
 }
 
 //class Teacher with TA as inheritance
 class Teacher{
   constructor(first, last, age, gender, interests, subject) {
-    this.name = {
-      first,
-      last
-    };
+    this.first = first;
+    this.last = last;
+    this.name = this.first + " " + this.last;
     this.age = age;
     this.gender = gender;
     this.interests = interests;
@@ -46,7 +48,7 @@ class Teacher{
 // inheritance from class Teacher
 class TA extends Teacher{
   constructor(first, last, age, gender, interest, subject, grade, year) {
-    super(first,last,age,gender,interest,subject);
+    super(first, last, age, gender, interest, subject);
     
     this.grade = grade;
     this.year = year;
@@ -63,11 +65,15 @@ class Schedule{
 }
 
 // class course werkt
+var timeslotA = new Schedule(["Monday","Wednessday"],["9-13","9-13"], "A");
+var timeslotB = new Schedule(["Tuesday","Thursday"],["9-13","9-13"], "B");
+var timeslotC = new Schedule(["monday","Thursday"],["13-17","9-13"], "C");
+var timeslotD = new Schedule(["monday","Thursday"],["13-17","9-13"], "D"); 
 
-var teacher1 = new Teacher("Remco","Veltkamp","50","male","Taking long walks on the beach","Gameprogrammeren");
+var teacher1 = new Teacher("Remco","Veltkamp","50","male","Taking long walks on the beach","Game programmeren");
 var teacher2 = new Teacher("Ad","Feelders","40","male","Mining away","Datamining");
 var teacher3 = new Teacher("Trevor","Mcdonell","28","male","Spending time with any dog, he really loves dogs","Concurency");
-var teacher4 = new Teacher("Silja","Renooij","35","female","Making robots","Gameprogrammeren");
+var teacher4 = new Teacher("Silja","Renooij","35","female","Making robots","Kunstmatige intelligentie");
 var teacher5 = new Teacher("Hans","Philipi","50","male","Talking","Databases");
 var teacher6 = new Teacher("Erik jan","van Leeuwen","30","male","Dynamic programming","Algoritmiek");
 var teacher7 = new Teacher("Gerard","Tel","60","male","Count sort","Datastructuren");
@@ -82,28 +88,36 @@ var ta6 = new TA("Ivo Gabe","de Wolff","22","male","Divide and conquer","Algorit
 var ta7 = new TA("Felix","Ant","21","male","Counting with gerard","Datastructuren","9","3");
 var ta8 = new TA("Mark","Dekker","21","male","Pixels","Graphics","9","3");
 
-var subject1 = new Courses("Gameprogrammeren", "INFOB1GP", "1", "7.5", "1",[teacher1.ta1]);
-var subject2 = new Courses("Datamining", "INFOMDM", "1", "7.5", "m",[teacher2,ta2]);
-var subject3 = new Courses("Concurrency", "INFOB3CC", "2", "7.5", "3",[teacher3,ta3]);
-var subject4 = new Courses("Kunstmatige intelligentie", "INFOB2KI", "2", "7.5", "2",[teacher4,ta4]);
-var subject5 = new Courses("Databases", "INFODB", "3", "7.5", "1",[teacher5,ta5]);
-var subject6 = new Courses("Algoritmiek", "INFOAL", "3", "7.5", "3",[teacher6,ta6]);
-var subject7 = new Courses("Datastructuren", "INFODS", "4", "7.5", "2",[teacher7,ta7]);
-var subject8 = new Courses("Graphics", "INFOGR", "4", "7.5", "2",[teacher8,ta8]);
+var subject1 = new Courses("Gameprogrammeren", "INFOB1GP", "1",timeslotA, "7.5", "1",[teacher1,ta1]);
+var subject2 = new Courses("Datamining", "INFOMDM", "1",timeslotB, "7.5", "m",[teacher2,ta2]);
+var subject3 = new Courses("Concurrency", "INFOB3CC", "2",timeslotC, "7.5", "3",[teacher3,ta3]);
+var subject4 = new Courses("Kunstmatige intelligentie", "INFOB2KI", "2",timeslotD, "7.5", "2",[teacher4,ta4]);
+var subject5 = new Courses("Databases", "INFODB", "3",timeslotA, "7.5", "1",[teacher5,ta5]);
+var subject6 = new Courses("Algoritmiek", "INFOAL", "3",timeslotB, "7.5", "3",[teacher6,ta6]);
+var subject7 = new Courses("Datastructuren", "INFODS", "4",timeslotC, "7.5", "2",[teacher7,ta7]);
+var subject8 = new Courses("Graphics", "INFOGR", "4",timeslotD, "7.5", "2",[teacher8,ta8]);
 
 //volgens mij bedoel je dit melwin ?
 var teacherAssistent = new TA('Voornaam', 'achternaam', '21', 'Male', 'reading', 'algortims', '4.5', '2020');
 var info = create('p');
-var teacher = createText(teacherAssistent.grade);
 
-append(info, teacher);
-append(body, info);
-// var teacherassistent = new TA('Algoritms', '4.5', '2020');
-// var info = create('p');
-// var teacher = createText(teacherassistent.grade);
-//
-// append(info, teacher);
+
+// appendText(info, teacherText(teacher1));
 // append(body, info);
+
+//let text = teacher[0].name + teacher[1].name
+let box = create("td");
+let tooltip = create("a");
+append(box,tooltip);
+tooltip.classList.add("tooltip")
+appendText(tooltip,subject1.title);
+
+let tooltipText = create("span");
+tooltipText.classList.add("tooltiptext");
+let text = teacherText(subject1.teacher)
+appendText(tooltipText, teacherText(subject1.teacher));
+append(tooltip,tooltipText);
+append(body,box);
 
 //creating table
 let table = create('table');
@@ -113,7 +127,7 @@ let tbody = create('tbody');
 function addTR (twhat,tag,tds) {
   
   let tr = create("tr");  
-
+  
   for (let text of tds) {
     let td = create(tag);
     appendText(td,text);
@@ -123,22 +137,58 @@ function addTR (twhat,tag,tds) {
   append(twhat,tr);
 }
 
-function addMultiple(twhat,tag, tds){
-   for (let ts of tds)
-    addTR(twhat,tag,ts);
+function addSubject(tbody,subject){
+  let tr = create("tr")
+  let tds = subjectToArray(subject);
+
+  createTooltip(subject.timeslot.timeslot,timeSlotText(subject.timeslot),tr);
+
+  for(let t of tds){
+    let td = create("td");
+    appendText(td,t);
+    append(tr,td);
+  }
+  
+  createTooltip(subject.title,teacherText(subject.teacher),tr);
+
+  append(tbody,tr);
+ }
+
+function addMultiple(twhat, subjects){
+   for (let subject of subjects)
+    addSubject(twhat,subject);
 }
 
+
+function teacherText(teacher){
+  let str = "Teacher: " + teacher[0].name + ", TA: " + teacher[1].name + ", TA grade: " + teacher[1].grade + ", TA year: " + teacher[1].year;
+  return str;
+}
+
+function timeSlotText(timeslot){
+  let str = timeslot.days[0] + ": " + timeslot.time[0] + " and " + timeslot.days[1] + ": " + timeslot.time[1];
+  return str;
+}
+
+function createTooltip(title,tiptext,parent){
+  let box = create("td");
+  let tooltip = create("a");
+  append(box,tooltip);
+  tooltip.classList.add("tooltip")
+  appendText(tooltip,title);
+
+  let tooltipText = create("span");
+  tooltipText.classList.add("tooltiptext");
+
+  appendText(tooltipText,tiptext);
+  append(tooltip,tooltipText);
+  append(parent,box);
+}
+
+subjectToArray = subject => [subject.period, subject.code, subject.nivo, subject.ects]
+
 addTR(thead,"th",["Period","Timeslot","Code","Nivo","ects", "vak"]);
-addMultiple(tbody,"td",[
-    [subject1.period, subject1.period, subject1.code, subject1.nivo, subject1.ects, subject1.title],
-    [subject2.period, subject2.period, subject2.code, subject2.nivo, subject2.ects, subject2.title],
-    [subject3.period, subject3.period, subject3.code, subject3.nivo, subject3.ects, subject3.title],
-    [subject4.period, subject4.period, subject4.code, subject4.nivo, subject4.ects, subject4.title],
-    [subject5.period, subject5.period, subject5.code, subject5.nivo, subject5.ects, subject5.title],
-    [subject6.period, subject6.period, subject6.code, subject6.nivo, subject6.ects, subject6.title],
-    [subject7.period, subject7.period, subject7.code, subject7.nivo, subject7.ects, subject7.title],
-    [subject8.period, subject8.period, subject8.code, subject8.nivo, subject8.ects, subject8.title],
-]);
+addMultiple(tbody,[subject1 ,subject2 ,subject3 ,subject4 ,subject5 ,subject6 ,subject7 ,subject8]);
 
 append(table,thead);
 append(table,tbody);
