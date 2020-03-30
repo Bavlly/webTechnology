ec2935c7068cde3cc58673851fdb3d6ae0ad1b8b
Bavlly Fanous <bavlly@backbase.com>
2020-03-30 13:11:43 +0200

    Cache + added sqlite3 database

---
+++ server/database.js
@@ -0,0 +1,17 @@
+const sqlite3 = require('sqlite3').verbose();
+
+// open database in memory
+let db = new sqlite3.Database('./db.sqlite3', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+});
+
+// close the database connections
+db.close((err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+});
